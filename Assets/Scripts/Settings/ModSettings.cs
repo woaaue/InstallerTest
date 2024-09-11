178873fead48d9072ce8253ce05780085f5a927e
woaaue <safat200@mail.ru>
2024-09-11 09:45:59 +0300

    edit mod settings

--- Assets/Scripts/Settings/ModSettings.cs
+++ Assets/Scripts/Settings/ModSettings.cs
@@ -1,15 +1,27 @@
 using System;
 using UnityEngine;
 using System.Linq;
+using UnityEditor;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "ModSettings", menuName = "Installer_Test/ModSettings", order = 1)]
 public sealed class ModSettings : ScriptableObject
 {
+    [field: SerializeField] public string Id { get; private set; }
     [field: SerializeField] public Sprite Icon { get; private set; }
+    [field: SerializeField] public List<Sprite> Screenshots { get; private set; }
     [field: SerializeField] public List<LanguageSettings> Languages { get; private set; }
 
     public LanguageSettings GetLanguageSettings(LanguageType languageType) => Languages.First(setting => setting.Type == languageType);
+
+    public void SetId()
+    {
+        Id = Guid.NewGuid().ToString();
+
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
 }
 
 [Serializable]
--- Assets/Scripts/Settings/ModsSettings.cs
+++ Assets/Scripts/Settings/ModsSettings.cs
@@ -1,8 +1,26 @@
 using UnityEngine;
+using NaughtyAttributes;
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "ModsSettings", menuName = "Installer_Test/ModsSettings", order = 1)]
 public sealed class ModsSettings : ScriptableObject
 {
     [field: SerializeField] public List<ModSettings> ModsSetting { get; private set; }
+
+#if UNITY_EDITOR
+    [Button("Generate id")]
+    public void GenerateId()
+    {
+        var hasChanges = false;
+
+        ModsSetting.ForEach(modSettings =>
+        {
+            if (string.IsNullOrEmpty(modSettings.Id))
+            {
+                modSettings.SetId();
+                hasChanges = true;
+            }
+        });
+    }
+#endif
 }
